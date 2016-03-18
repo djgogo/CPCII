7f337706511a08695bf906350d1845b02455435a
Peter Sacco <peter.sacco@competec.ch>
2016-03-18 02:15:09 +0100

    diceGame with UnitTests

--- CardGame/tests/DiceGameTest.php
+++ CardGame/tests/DiceGameTest.php
@@ -37,49 +37,61 @@ class DiceGameTest extends PHPUnit_Framework_TestCase
 
     public function testPlayGameWorks()
     {
+        // create a mock Player
         $player = $this->getMockBuilder(Player::class)
             ->disableOriginalConstructor()
             ->getMock();
 
+        // add the mock to the DiceGame
         $this->diceGame->addPlayer($player);
 
+        // make sure method rollDiceAndTurnMatchingCard is called
         $player->expects($this->once())
             ->method('rollDiceAndTurnMatchingCard');
 
+        // make method hasAllCardsTurned return true
         $player
             ->expects($this->once())
             ->method('hasAllCardsTurned')
             ->will($this->returnValue(true));
 
+        // EXECUTE method
         $this->diceGame->playGame();
     }
 
     public function testPlayGameWithTwoPlayersWorks()
     {
+        // create mock Player1
         $player1 = $this->getMockBuilder(Player::class)
             ->disableOriginalConstructor()
             ->getMock();
 
+        // create mock Player2
         $player2 = $this->getMockBuilder(Player::class)
             ->disableOriginalConstructor()
             ->getMock();
 
+        // add both mocks to the DiceGame
         $this->diceGame->addPlayer($player1);
         $this->diceGame->addPlayer($player2);
 
+        // make sure method rollDiceAndTurnMatchingCard is called
         $player1
             ->expects($this->once())
             ->method('rollDiceAndTurnMatchingCard');
 
+        // make method hasAllCardsTurned return false
         $player1
             ->expects($this->once())
             ->method('hasAllCardsTurned')
             ->will($this->returnValue(false));
 
+        // make sure method rollDiceAndTurnMatchingCard is called
         $player2
             ->expects($this->once())
             ->method('rollDiceAndTurnMatchingCard');
 
+        // make method hasAllCardsTurned return true
         $player2
             ->expects($this->once())
             ->method('hasAllCardsTurned')
--- CardGame/tests/PlayerTest.php
+++ CardGame/tests/PlayerTest.php
@@ -1,10 +1,9 @@
 <?php
 
-
 class PlayerTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @var Dice
+     * @var PHPUnit_Framework_MockObject_MockObject
      */
     private $dice;
     /**
@@ -19,6 +18,14 @@ class PlayerTest extends PHPUnit_Framework_TestCase
      * @var string
      */
     private $name;
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $card;
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $color;
 
     public function setUp()
     {
@@ -27,7 +34,14 @@ class PlayerTest extends PHPUnit_Framework_TestCase
         $this->dice = $this->getMockBuilder(Dice::class)
             ->disableOriginalConstructor()
             ->getMock();
+        $this->color = $this->getMockBuilder(Color::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->card = $this->getMockBuilder(Card::class)
+            ->disableOriginalConstructor()
+            ->getMock();
         $this->player = new Player($this->name, $this->dice, $this->logger);
+        $this->player->addCard($this->card);
     }
 
     public function testGetName()
@@ -37,7 +51,61 @@ class PlayerTest extends PHPUnit_Framework_TestCase
 
     public function testRollDiceAndTurnMatchingCardWorks()
     {
-        
+        // roll the dice once and get diced color
+        $this->dice->expects($this->once())
+            ->method('roll')
+            ->will($this->returnValue($this->color));
+
+        // let the comparison get true with same color
+        $this->card->expects($this->once())
+            ->method('getColor')
+            ->will($this->returnValue($this->color));
+
+        // make sure the method turn is called
+        $this->card->expects($this->once())
+            ->method('turn');
+
+        // EXECUTE method
+        $this->player->rollDiceAndTurnMatchingCard();
+    }
+
+    public function testRollDiceAndColorDoesNotMatchCardWorks()
+    {
+        // roll the dice once and get diced color
+        $this->dice->expects($this->once())
+            ->method('roll')
+            ->will($this->returnValue($this->color));
+
+        // create wrong color for the negate test
+        $wrongColor = $this->getMockBuilder(Color::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // let the comparison fail with the wrong color
+        $this->card->expects($this->once())
+            ->method('getColor')
+            ->will($this->returnValue($wrongColor));
+
+        // make sure turn is not called if wrong color
+        $this->card->expects($this->never())
+            ->method('turn');
+
+        // Card is not revealed, so log the message
+        $this->card->expects($this->once())
+            ->method('isRevealed')
+            ->will($this->returnValue(false));
+
+        // EXECUTE method
+        $this->player->rollDiceAndTurnMatchingCard();
+    }
+
+    public function testHasAllCardsTurnedWorks()
+    {
+        // TODO this last test!!!
+        $this->card->expects($this->once())
+            ->method('isRevealed')
+            ->will($this->returnValue(true));
+
     }
 
 }
