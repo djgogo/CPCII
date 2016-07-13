c0a8751e01d4d1abcc309860d83aa99ed785c69b
Peter Sacco <peter.sacco@competec.ch>
2016-07-13 14:10:14 +0200

    Auction

--- Auction/public/index.php
+++ Auction/public/index.php
@@ -1,8 +1,8 @@
 <?php
 $_SERVER['REQUEST_METHOD'] = 'POST';
 $_SERVER['REQUEST_URI'] = '/bid';
-$post['auction'] = '1';
-$post['amount'] = '100';
+$_POST['auction'] = '1';
+$_POST['amount'] = '100';
 
 require __DIR__ . '/../src/autoload.php';
 
