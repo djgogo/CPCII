ddafede207aab571cf33f395de4b378738e24162
Peter Sacco <peter.sacco@competec.ch>
2016-09-07 09:22:30 +0200

    Security - TheSuXXStore Exercise

---
+++ TheSuXXStore/html/classes/factory.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This code is part of the Suxx security demo application
+ *
+ * *** DO NOT USE IN ANY TYPE OF PRODUCTION ***
+ *
+ * The application is stripped down and contains various security issues to be found
+ * by course attendees. It is not meant to be used as an actual shop application or a
+ * base for one.
+ *
+ * @author Arne Blankerts <arne.blankerts@thephp.cc>
+ * @copyright 2011-2012 thePHP.cc - The PHP Consulting Company, Germany
+ *
+ */
+class SuxxFactory
+{
+    protected $defaultController = 'SuxxErrorController';
+
+    public function setDefaultController($default)
+    {
+        $this->defaultController = $default;
+    }
+
+    public function getController($name)
+    {
+        $name = 'Suxx' . ucfirst($name) . 'Controller';
+        if (!class_exists($name)) {
+            return new $this->defaultController($this);
+        }
+        return new $name($this);
+    }
+
+    public function getDatabase($dsn)
+    {
+        return new SuxxDatabase($dsn);
+    }
+}
---
+++ TheSuXXStore/html/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This code is part of the Suxx security demo application
+ *
+ * *** DO NOT USE IN ANY TYPE OF PRODUCTION ***
+ *
+ * The application is stripped down and contains various security issues to be found
+ * by course attendees. It is not meant to be used as an actual shop application or a
+ * base for one.
+ *
+ * @author Arne Blankerts <arne.blankerts@thephp.cc>
+ * @copyright 2011-2012 thePHP.cc - The PHP Consulting Company, Germany
+ *
+ */
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+define('DSN', 'mysql://root@127.0.0.1/suxx');
+
+require __DIR__ . '/autoload.php';
+session_start();
+
+$request  = new SuxxRequest($_REQUEST, $_SESSION);
+$response = new SuxxResponse();
+$factory  = new SuxxFactory();
+
+$suxx = new SuxxFrontController($request, $response, $factory);
+$view = $suxx->execute(isset($_GET["controller"]) ? $_GET['controller'] : 'home');
+
+echo $view->render($request, $response);
