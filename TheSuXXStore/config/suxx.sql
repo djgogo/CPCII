337db752ba15b63e3cd1d2b629cf597c4b0f5d63
Peter Sacco <peter.sacco@competec.ch>
2016-11-08 08:36:12 +0100

    MySql-Dump added

--- TheSuXXStore/config/suxx.sql
+++ TheSuXXStore/config/suxx.sql
@@ -3,12 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 25. Okt 2016 um 08:34
+-- Erstellungszeit: 08. Nov 2016 um 08:34
 -- Server-Version: 5.5.51
 -- PHP-Version: 7.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Datenbank: `suxx`
@@ -23,24 +29,34 @@ USE `suxx`;
 --
 
 CREATE TABLE `comments` (
-  `CID` int(10) UNSIGNED NOT NULL,
-  `PID` int(10) UNSIGNED NOT NULL,
-  `AUTHOR` varchar(200) NOT NULL,
-  `COMMENT` text NOT NULL,
-  `PICTURE` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `cid` int(10) UNSIGNED NOT NULL,
+  `pid` int(10) UNSIGNED NOT NULL,
+  `author` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `comment` text CHARACTER SET utf8 NOT NULL,
+  `picture` varchar(200) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `comments`
 --
 
-INSERT INTO `comments` (`CID`, `PID`, `AUTHOR`, `COMMENT`, `PICTURE`) VALUES
-(1, 1, 'gogo', 'This is my absolutely most favourite Headphone ever :-)', ''),
+INSERT INTO `comments` (`cid`, `pid`, `author`, `comment`, `picture`) VALUES
+(2, 1, 'gogo', 'This is my absolutely most favourite Headphone ever :-)', ''),
 (8, 1, 'gogo', 'one more time', 'smiley.jpg'),
 (9, 1, 'test', 'test comment Bla Bla Bla!\r\nBla Bla Bla\r\nTest Test Test', ''),
 (10, 2, 'gogo', 'amazinggreatsupercoolfabulous... :-)', ''),
 (11, 1, 'test', 'test test', ''),
-(12, 2, 'petersacco', 'test ', 'smiley.jpg');
+(12, 2, 'petersacco', 'test ', 'smiley.jpg'),
+(13, 1, 'petersacco', '', ''),
+(14, 1, 'petersacco', 'Test Photo ', 'smiley.jpg'),
+(15, 1, 'petersacco', 'Kommentar 1', ''),
+(16, 1, 'petersacco', 'Kommentar 2', ''),
+(17, 1, 'petersacco', 'Kommentar 3', ''),
+(18, 2, 'petersacco', 'Kommentar 1', ''),
+(19, 2, 'petersacco', 'Kommentar 2', ''),
+(20, 2, 'petersacco', 'Kommentar 3', ''),
+(21, 2, 'suxxuser', 'Kommentar von SuxxUser', ''),
+(22, 2, 'suxxuser', 'Smiley von the Suxx', 'smiley.jpg');
 
 -- --------------------------------------------------------
 
@@ -49,26 +65,21 @@ INSERT INTO `comments` (`CID`, `PID`, `AUTHOR`, `COMMENT`, `PICTURE`) VALUES
 --
 
 CREATE TABLE `products` (
-  `PID` int(10) NOT NULL,
-  `LABEL` varchar(200) NOT NULL,
-  `IMG` varchar(200) NOT NULL,
-  `PRICE` int(11) NOT NULL,
-  --
-  -- automatic initializing and updating for more than one timestamp field is not possible before MySQL 5.6.5 !!!!
-  --
-  -- `CREATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  -- `UPDATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  `CREATED` DATETIME NOT NULL,
-  `UPDATED` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `pid` int(10) NOT NULL,
+  `label` varchar(200) NOT NULL,
+  `img` varchar(200) NOT NULL,
+  `price` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `products`
 --
 
-INSERT INTO `products` (`PID`, `LABEL`, `IMG`, `PRICE`) VALUES
-(1, 'Technics RP DJ 1200 Headphones', 'Technics_RP_DJ_1210.jpg', 115),
-(2, 'Technics SL-1210 Plattenspieler', 'technics_sl1210mk2.jpg', 1250);
+INSERT INTO `products` (`pid`, `label`, `img`, `price`, `created`, `updated`) VALUES
+(1, 'Technics RP DJ 1210', 'Technics_RP_DJ_1210.jpg', 150, '2016-10-26 11:00:00', '2016-11-07 16:03:16'),
+(2, 'Technics SL-1210 Plattenspieler', 'technics_sl1210mk2.jpg', 1260, '2016-10-31 15:11:28', '2016-11-04 14:59:32');
 
 -- --------------------------------------------------------
 
@@ -92,22 +103,28 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`UID`, `USERNAME`, `PASSWD`, `EMAIL`, `NAME`, `DESCR`, `PICTURE`, `CREATED`) VALUES
-(1, 'petersacco', '$2y$10$H81dsPPsAbkT10oShO5AruHZsobbzAL5c7urFXg1dBZEzvSXPWGfi', 'peter@sacco.ch', 'Peter Sacco', 'pwd 1234', '', '2016-10-21 10:53:42'),
-(2, 'gogo', '$2y$10$2SR0l.bZHmDEU/Flhr8gxeSFDlryH3WuLYw73dqIXoegXl/Qj0T9O', 'gogo@musiq.ch', 'Gogo', 'pwd 123456', '', '2016-10-21 15:05:50'),
-(3, 'test', '$2y$10$SdxetgoS9CyCdcjq8RTY4Owiz0BoaT57YV7QkbKlizAdDWXyZttim', 'test@muster.ch', 'Test Muster', 'pwd 123456', '', '2016-10-24 08:05:09');
+(10, 'petersacco', '$2y$10$H81dsPPsAbkT10oShO5AruHZsobbzAL5c7urFXg1dBZEzvSXPWGfi', 'peter@sacco.ch', 'Peter Sacco', 'Suxx Account', '', '2016-10-21 10:53:42'),
+(11, 'gogo', '$2y$10$2SR0l.bZHmDEU/Flhr8gxeSFDlryH3WuLYw73dqIXoegXl/Qj0T9O', 'gogo@musiq.ch', 'Gogo', 'Suxx Account', '', '2016-10-21 15:05:50'),
+(12, 'test', '$2y$10$SdxetgoS9CyCdcjq8RTY4Owiz0BoaT57YV7QkbKlizAdDWXyZttim', 'test@muster.ch', 'Test Muster', 'Suxx Account', '', '2016-10-24 08:05:09'),
+(14, 'suxxuser', '$2y$10$GGa3aBaoARbvgyX0N4Lh4ejGXMV3tAU5Ui.kVSh6O1rSfhr9VH./6', 'suxx@user.net', 'Suxx User', 'Suxx Account', '', '2016-10-26 12:32:18'),
+(15, 'suxx', '$2y$10$1VPd/2yfNKlcIxQeb8s8xu/cdVgUbr7oZjbXh/YTKZR7jHFC8tXM6', 'suxx@master.com', 'Suxx Master', 'Suxx Account', '', '2016-11-01 09:10:55');
+
+--
+-- Indizes der exportierten Tabellen
+--
 
 --
 -- Indizes für die Tabelle `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`CID`),
-  ADD KEY `PID` (`PID`);
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `PID` (`pid`);
 
 --
 -- Indizes für die Tabelle `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`PID`);
+  ADD PRIMARY KEY (`pid`);
 
 --
 -- Indizes für die Tabelle `user`
@@ -117,19 +134,24 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `USERNAME` (`USERNAME`);
 
 --
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
 -- AUTO_INCREMENT für Tabelle `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `CID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+  MODIFY `cid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- AUTO_INCREMENT für Tabelle `products`
 --
 ALTER TABLE `products`
-  MODIFY `PID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `pid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
